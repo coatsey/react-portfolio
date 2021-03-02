302397e0eb3452e5b8c54a70a4bb3f4e24e8c0c8
Logan Coates <coates.logan1125@gmail.com>
2021-03-02 14:14:56 -0700

    finished contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import About from '../components/About';
+import Golf from "../assets/images/email.png";
+import Github from "../assets/images/github.jpg";
+import Linkedin from "../assets/images/Linkedin.png";
+
 
 function Contact(props) {
     
-    return(
-        <p>contact works!</p>
-    );
-}
+    return (
+        <div>
+          <About title={props.title} />
+    
+          <img className="contact-img" src={Golf} alt="Me" />
+          <p className="p-tag">Email:Coates.logan1125@gmail.com</p>
+          <img className="github-img" src={Github} alt="Me" />
+          <p className="p-tag"><a href="https://github.com/coatsey"> Github</a></p>
+          <img className="linkedin-img" src={Linkedin} alt="Me" />
+          <p className="p-tag"><a href="https://www.linkedin.com/in/logan-coates-b404b61bb/"> Linkedin</a></p>
+        </div>
+      );
+    }
 
 export default Contact;
